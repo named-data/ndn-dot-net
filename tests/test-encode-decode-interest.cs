69ec0ca61f99ac6556ffc07b2aeec083728cd6ee
Jeff Thompson <jefft0@gmail.com>
2014-02-18 13:30:43 -0800

    tests: In test-encode-decode-interest, use byte, not sbyte.

--- tests/test-encode-decode-interest.cs
+++ tests/test-encode-decode-interest.cs
@@ -78,7 +78,7 @@ namespace TestNdnDotNet
             freshInterest.setMinSuffixComponents(4);
             freshInterest.setMaxSuffixComponents(6);
             freshInterest.getPublisherPublicKeyDigest().setPublisherPublicKeyDigest
-                (new Blob(new sbyte[] {
+                (new Blob(new byte[] {
                  0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 
                  0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F }));
             freshInterest.getExclude().appendComponent(new Name("abc").get(0)).appendAny();
