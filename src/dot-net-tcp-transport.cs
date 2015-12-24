3cd7803d9ec6998fd06ccd7e5b0e291adcef1ee0
Jeff Thompson <jefft0@remap.ucla.edu>
2015-12-23 16:57:18 -0800

    Updated DotNetTcpTransport.

--- src/dot-net-tcp-transport.cs
+++ src/dot-net-tcp-transport.cs
@@ -4,12 +4,13 @@
  * See COPYING for copyright and distribution information.
  */
 
+using ILOG.J2CsMapping.NIO;
 using System;
 using System.Net;
 using System.Net.Sockets;
-using java.nio;
 using net.named_data.jndn;
 using net.named_data.jndn.encoding;
+using net.named_data.jndn.util;
 
 namespace net.named_data.jndn.transport
 {
@@ -67,6 +68,38 @@ namespace net.named_data.jndn.transport
     }
 
     /// <summary>
+    /// Determine whether this transport connecting according to connectionInfo is
+    /// to a node on the current machine. This affects the processing of
+    /// Face.registerPrefix(): if the NFD is local, registration occurs with the
+    /// '/localhost/nfd...' prefix; if non-local, the library will attempt to use
+    /// remote prefix registration using '/localhop/nfd...'
+    /// </summary>
+    ///
+    /// <param name="connectionInfo">A ConnectionInfo with the host to check.</param>
+    /// <returns>True if the host is local, false if not.</returns>
+    public override bool 
+    isLocal(Transport.ConnectionInfo connectionInfo)
+    {
+      if (connectionInfo_ == null || 
+          ((ConnectionInfo)connectionInfo).getHost() !=  connectionInfo_.getHost()) {
+        isLocal_ = getIsLocal(((ConnectionInfo)connectionInfo).getHost());
+        connectionInfo_ = (ConnectionInfo)connectionInfo;
+      }
+
+      return isLocal_;
+    }
+
+    /// <summary>
+    /// Override to return false since connect does not need to use the onConnected
+    /// callback.
+    /// </summary>
+    ///
+    /// <returns>False.</returns>
+    public override bool isAsync() {
+      return false;
+    }
+
+    /// <summary>
     /// This is from the code sample at
     /// http://msdn.microsoft.com/en-us/library/system.net.sockets.socket(v=vs.110).aspx
     /// </summary>
@@ -103,11 +136,14 @@ namespace net.named_data.jndn.transport
     /// <summary>
     /// Connect according to the info in ConnectionInfo, and use elementListener.
     /// </summary>
-    /// <param name="connectionInfo">A DotNetTcpTransport.ConnectionInfo.</param>
-    /// <param name="elementListener">The ElementListener must remain valid during the 
-    /// life of this object.</param>
-    public override void
-    connect(Transport.ConnectionInfo connectionInfo, ElementListener elementListener)
+    ///
+    /// <param name="connectionInfo">A TcpTransport.ConnectionInfo.</param>
+    /// <param name="elementListener"></param>
+    /// <param name="onConnected"></param>
+    /// <exception cref="IOException">For I/O error.</exception>
+    public override void 
+    connect(Transport.ConnectionInfo connectionInfo,
+      ElementListener elementListener, IRunnable onConnected) 
     {
       close();
 
@@ -115,7 +151,10 @@ namespace net.named_data.jndn.transport
         (((ConnectionInfo)connectionInfo).getHost(),
          ((ConnectionInfo)connectionInfo).getPort());
 
-      elementReader_ = new BinaryXmlElementReader(elementListener);
+      elementReader_ = new ElementReader(elementListener);
+
+      if (onConnected != null)
+        onConnected.run();
     }
 
     /// <summary>
@@ -199,9 +238,29 @@ namespace net.named_data.jndn.transport
       }
     }
 
+    /// <summary>
+    /// A static method to determine whether the host is on the current machine.
+    /// Results are not cached. According to
+    /// http://redmine.named-data.net/projects/nfd/wiki/ScopeControl#local-face,
+    /// TCP transports with a loopback address are local. If connectionInfo
+    /// contains a host name, InetAddress will do a blocking DNS lookup; otherwise
+    /// it will parse the IP address and examine the first octet to determine if
+    /// it is a loopback address (e.g. first octet == 127).
+    /// </summary>
+    ///
+    /// <param name="host">The host to check.</param>
+    /// <returns>True if the host is local, False if not.</returns>
+    public static bool 
+    getIsLocal(string host) 
+    {
+      // TODO: Implement the lookup as in TcpTransport.
+      return false;
+    }
+
     Socket socket_ = null;
     ByteBuffer inputBuffer_ = ByteBuffer.allocate(8000);
-    // TODO: This belongs in the socket listener.
-    private BinaryXmlElementReader elementReader_;
+    private ElementReader elementReader_;
+    private ConnectionInfo connectionInfo_;
+    private bool isLocal_;
   }
 }
