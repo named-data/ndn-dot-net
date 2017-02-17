70cfe68500ca0ba8df1d308fc72a160036cd7ef0
Jeff Thompson <jefft0@remap.ucla.edu>
2017-02-17 12:20:06 -0800

    tests: Split out JUnit.runTests into a separate utility class.

---
+++ tests/junit-dot-net.cs
@@ -0,0 +1,78 @@
+﻿/**
+ * Copyright (C) 2017 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+using System;
+using System.Reflection;
+
+namespace JUnitDotNet {
+  public class JUnit {
+    /// <summary>
+    /// This imitates the behavior of JUnit. to use reflection to find all classes 
+    /// in the given assembly in the given nameSpace where the class name starts with 
+    /// "Test". Run methods which start with "test". If the class defines a "setUp"
+    /// method, run it before each test method. If the class defines a "tearDown"
+    /// method, run it after each test method.
+    /// Assume the test will use the Assert methods that may throw an exception.
+    /// Print results to Console.Out.
+    /// </summary>
+    public static void 
+    runTests(Assembly assembly, string nameSpace)
+    {
+      int nFailed = 0;
+
+      // Find all types in the unit_tests namespace that start with "Test".
+      foreach (var type in assembly.GetTypes()) {
+        if (type.Namespace == nameSpace && type.Name.StartsWith("Test")) {
+          var testInstance = type.GetConstructor(new Type[0]).Invoke(null);
+
+          var setUpMethod = type.GetMethod("setUp", new Type[0]);
+          var tearDownMethod = type.GetMethod("tearDown", new Type[0]);
+
+          // Find each method that starts with "test" and takes no arguments.
+          foreach (var methodInfo in type.GetMethods()) {
+            if (!methodInfo.IsStatic && methodInfo.Name.StartsWith("test") &&
+                methodInfo.GetParameters().Length == 0) {
+              Console.Out.WriteLine("Running test " + type.Name + "." + methodInfo.Name);
+
+              if (setUpMethod != null)
+                setUpMethod.Invoke(testInstance, null);
+
+              // Invoke the test and print any error.
+              try {
+                methodInfo.Invoke(testInstance, null);
+              } catch (TargetInvocationException ex) {
+                ++nFailed;
+                Console.Out.WriteLine("FAIL: " + ex.InnerException.Message);
+              }
+
+              if (tearDownMethod != null)
+                tearDownMethod.Invoke(testInstance, null);
+            }
+          }
+        }
+      }
+
+      Console.Out.WriteLine("");
+      if (nFailed > 0)
+        Console.Out.WriteLine("FAILED " + nFailed + " tests.");
+      else
+        Console.Out.WriteLine("PASSED all tests.");
+    }
+  }
+}
--- tests/run-unit-tests.cs
+++ tests/run-unit-tests.cs
@@ -1,5 +1,5 @@
 ﻿/**
- * Copyright (C) 2016 Regents of the University of California.
+ * Copyright (C) 2016-2017 Regents of the University of California.
  * @author: Jeff Thompson <jefft0@remap.ucla.edu>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -18,13 +18,12 @@
  */
 
 using System;
-using System.Reflection;
+using JUnitDotNet;
 
 namespace TestNdnDotNet {
   class RunUnitTests {
     /// <summary>
-    /// Use reflection to find and run tests in the namespace 
-    /// net.named_data.jndn.tests.unit_tests.
+    /// Run tests in the namespace net.named_data.jndn.tests.unit_tests.
     /// </summary>
     static void Main(string[] args)
     {
@@ -32,44 +31,7 @@ namespace TestNdnDotNet {
 
       // Use TestNameConventions just to get its Assembly.
       var assembly = typeof(net.named_data.jndn.tests.unit_tests.TestNameConventions).Assembly;
-      // Find all types in the unit_tests namespace that start with "Test".
-      foreach (var type in assembly.GetTypes()) {
-        if (type.Namespace == "net.named_data.jndn.tests.unit_tests" &&
-            type.Name.StartsWith("Test")) {
-          var testInstance = type.GetConstructor(new Type[0]).Invoke(null);
-
-          var setUpMethod = type.GetMethod("setUp", new Type[0]);
-          var tearDownMethod = type.GetMethod("tearDown", new Type[0]);
-
-          // Find each method that starts with "test" and takes no arguments.
-          foreach (var methodInfo in type.GetMethods()) {
-            if (!methodInfo.IsStatic && methodInfo.Name.StartsWith("test") &&
-                methodInfo.GetParameters().Length == 0) {
-              Console.Out.WriteLine("Running test " + type.Name + "." + methodInfo.Name);
-
-              if (setUpMethod != null)
-                setUpMethod.Invoke(testInstance, null);
-
-              // Invoke the test and print any error.
-              try {
-                methodInfo.Invoke(testInstance, null);
-              } catch (TargetInvocationException ex) {
-                ++nFailed;
-                Console.Out.WriteLine("FAIL: " + ex.InnerException.Message);
-              }
-
-              if (tearDownMethod != null)
-                tearDownMethod.Invoke(testInstance, null);
-            }
-          }
-        }
-      }
-
-      Console.Out.WriteLine("");
-      if (nFailed > 0)
-        Console.Out.WriteLine("FAILED " + nFailed + " tests.");
-      else
-        Console.Out.WriteLine("PASSED all tests.");
+      JUnit.runTests(assembly, "net.named_data.jndn.tests.unit_tests");
     }
   }
 }
