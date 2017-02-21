28ffb137ba1af0eceb03ccf9f895e15ad94e91dd
Jeff Thompson <jefft0@remap.ucla.edu>
2017-02-21 12:13:15 -0800

    tests: Added run-integration-tests.cs.

---
+++ tests/run-integration-tests.cs
@@ -0,0 +1,37 @@
+﻿/**
+ * Copyright (C) 2016-2017 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+using System;
+using JUnitDotNet;
+
+namespace TestNdnDotNet {
+  class RunUnitTests {
+    /// <summary>
+    /// Run tests in the namespace net.named_data.jndn.tests.integration_tests.
+    /// </summary>
+    static void Main(string[] args)
+    {
+      int nFailed = 0;
+
+      // Use TestFaceInterestMethods just to get its Assembly.
+      var assembly = typeof(net.named_data.jndn.tests.integration_tests.TestFaceInterestMethods).Assembly;
+      JUnit.runTests(assembly, "net.named_data.jndn.tests.integration_tests");
+    }
+  }
+}
