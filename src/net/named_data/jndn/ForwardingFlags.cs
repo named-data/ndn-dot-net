0fc2ad872e30bade9a5a484006f5ef7bfe6cc992
Jeff Thompson <jefft0@remap.ucla.edu>
2019-04-05 12:54:15 -0700

    Deprecated ForwardingFlags and make it inherit from RegistrationOptions. Refs #4869.

--- src/net/named_data/jndn/ForwardingFlags.cs
+++ src/net/named_data/jndn/ForwardingFlags.cs
@@ -16,23 +16,13 @@ namespace net.named_data.jndn {
 	using System.IO;
 	using System.Runtime.CompilerServices;
 	
-	/// <summary>
-	/// A ForwardingFlags object holds the flags which specify how the forwarding
-	/// daemon should forward an interest for a registered prefix.  We use a separate
-	/// ForwardingFlags object to retain future compatibility if the daemon
-	/// forwarding bits are changed, amended or deprecated.
-	/// </summary>
-	///
-	public class ForwardingFlags {
+	public class ForwardingFlags : RegistrationOptions {
 		/// <summary>
 		/// Create a new ForwardingFlags with "childInherit" set and all other flags
 		/// cleared.
 		/// </summary>
 		///
 		public ForwardingFlags() {
-			this.childInherit_ = true;
-			this.capture_ = false;
-			this.origin_ = -1;
 		}
 	
 		/// <summary>
@@ -40,112 +30,7 @@ namespace net.named_data.jndn {
 		/// </summary>
 		///
 		/// <param name="forwardingFlags">The ForwardingFlags to copy.</param>
-		public ForwardingFlags(ForwardingFlags forwardingFlags) {
-			this.childInherit_ = true;
-			this.capture_ = false;
-			this.origin_ = -1;
-			childInherit_ = forwardingFlags.childInherit_;
-			capture_ = forwardingFlags.capture_;
-			origin_ = forwardingFlags.origin_;
+		public ForwardingFlags(RegistrationOptions forwardingFlags) : base(forwardingFlags) {
 		}
-	
-		/// <summary>
-		/// Get the value of the "childInherit" flag.
-		/// </summary>
-		///
-		/// <returns>true if the flag is set, false if it is cleared.</returns>
-		public bool getChildInherit() {
-			return childInherit_;
-		}
-	
-		/// <summary>
-		/// Get the value of the "capture" flag.
-		/// </summary>
-		///
-		/// <returns>true if the flag is set, false if it is cleared.</returns>
-		public bool getCapture() {
-			return capture_;
-		}
-	
-		/// <summary>
-		/// Get the origin value.
-		/// </summary>
-		///
-		/// <returns>The origin value, or -1 if not specified.</returns>
-		public int getOrigin() {
-			return origin_;
-		}
-	
-		/// <summary>
-		/// Set the value of the "childInherit" flag
-		/// </summary>
-		///
-		/// <param name="childInherit">true to set the flag, false to clear it.</param>
-		/// <returns>This ForwardingFlags so that you can chain calls to update values.</returns>
-		public ForwardingFlags setChildInherit(bool childInherit) {
-			childInherit_ = childInherit;
-			return this;
-		}
-	
-		/// <summary>
-		/// Set the value of the "capture" flag
-		/// </summary>
-		///
-		/// <param name="capture">true to set the flag, false to clear it.</param>
-		/// <returns>This ForwardingFlags so that you can chain calls to update values.</returns>
-		public ForwardingFlags setCapture(bool capture) {
-			capture_ = capture;
-			return this;
-		}
-	
-		/// <summary>
-		/// Set the origin value.
-		/// </summary>
-		///
-		/// <param name="origin">The new origin value, or -1 for not specified.</param>
-		/// <returns>This ForwardingFlags so that you can chain calls to update values.</returns>
-		public ForwardingFlags setOrigin(int origin) {
-			origin_ = origin;
-			return this;
-		}
-	
-		/// <summary>
-		/// Get an integer with the bits set according to the NFD forwarding flags as
-		/// used in the ControlParameters of the command interest.
-		/// This ignores the origin value.
-		/// </summary>
-		///
-		/// <returns>An integer with the bits set.</returns>
-		public int getNfdForwardingFlags() {
-			int result = 0;
-	
-			if (childInherit_)
-				result |= NfdForwardingFlags_CHILD_INHERIT;
-			if (capture_)
-				result |= NfdForwardingFlags_CAPTURE;
-	
-			return result;
-		}
-	
-		/// <summary>
-		/// Set the flags according to the NFD forwarding flags as used in the
-		/// ControlParameters of the command interest.
-		/// </summary>
-		///
-		/// <param name="nfdForwardingFlags">An integer with the bits set.</param>
-		/// <returns>This ForwardingFlags so that you can chain calls to update values.</returns>
-		public ForwardingFlags setNfdForwardingFlags(int nfdForwardingFlags) {
-			childInherit_ = (nfdForwardingFlags & NfdForwardingFlags_CHILD_INHERIT) != 0;
-			capture_ = (nfdForwardingFlags & NfdForwardingFlags_CAPTURE) != 0;
-			return this;
-		}
-	
-		private const int NfdForwardingFlags_CHILD_INHERIT = 1;
-		private const int NfdForwardingFlags_CAPTURE = 2;
-	
-		private bool childInherit_;
-		private bool capture_;
-		private int origin_;
-		/**< -1 for none. */
 	}
 }
